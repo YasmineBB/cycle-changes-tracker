fd0d7016a4b7328412c6bf157ef6d32231b95687
YasmineBB <y.bourhabi@gmail.com>
2022-03-22 14:44:49 +0000

    Connect APIs and confirm access to google sheets

--- run.py
+++ run.py
@@ -1,3 +1,24 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('cycle_changes_tracker')
+
+""" Add sheets from worksheet """
+
+symptoms = SHEET.worksheet('symptoms')
+
+data = symptoms.get_all_values()
+
+print(data)
