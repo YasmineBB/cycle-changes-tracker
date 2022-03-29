68a8b129a7cb8300f510a9ebfdca712bd4ff7607
YasmineBB <y.bourhabi@gmail.com>
2022-03-29 14:10:43 +0000

    Write function that gets the users name

--- run.py
+++ run.py
@@ -17,8 +17,21 @@ SHEET = GSPREAD_CLIENT.open('cycle_changes_tracker')
 
 """ Add sheets from worksheet """
 
-symptoms = SHEET.worksheet('symptoms')
+# symptoms = SHEET.worksheet('symptoms')
 
-data = symptoms.get_all_values()
+# data = symptoms.get_all_values()
 
-print(data)
+# print(data)
+
+def get_user_name():
+    """
+    Provides a brief introduction to the user and gets the users name.
+    """
+    print('Hello!\n')
+    print('Welcome to Cycle Changes Tracker...\n')
+    print('We are here to provide you with that extra support with tracking any changes to your symptoms after changing your method of birth control.\n')
+
+    name = input('Before we start, lets get acquainted. Please enter your name: ')
+    print(f'Lovely to meet you, {name}!\n')
+
+get_user_name()
