c4f4394462c00bc24f40524a68b7872b578150fb
YasmineBB <y.bourhabi@gmail.com>
2022-03-30 17:40:05 +0000

    Add title to start page and create cycle_phase function that asks whether the user is on their period or not. They will be asked questions depending on answer.

--- run.py
+++ run.py
@@ -25,6 +25,8 @@ SHEET = GSPREAD_CLIENT.open('cycle_changes_tracker')
 
 # print(data)
 
+
+
 """
 Functions to add typewriter effect.
 Taken from www.101computing.net/python-typing-text-effect/
@@ -35,7 +37,6 @@ def typingPrint(text):
     sys.stdout.write(character)
     sys.stdout.flush()
     time.sleep(0.05)
-  
 def typingInput(text):
   for character in text:
     sys.stdout.write(character)
@@ -54,13 +55,57 @@ def get_user_name():
     """
     typingPrint('Hello...\n')
     time.sleep(1)
-    typingPrint('Welcome to ' + Fore.MAGENTA + 'Cycle Changes Tracker!\n'+ Fore.RESET)
+    typingPrint('Welcome to ' + Fore.LIGHTMAGENTA_EX + 'Cycle Changes Tracker!\n' + Fore.RESET)
     time.sleep(1)
     typingPrint('We are here to provide you with that extra support with tracking any changes to your symptoms after changing your method of birth control.\n')
     time.sleep(1)
 
-    name = input('Before we start, lets get acquainted. Please enter your name:\n')
+    typingPrint('Before we start, lets get acquainted... \n')
     time.sleep(1)
-    typingPrint(f'Lovely to meet you, ' + Fore.MAGENTA + f'{name}!\n')
+    name = typingInput('Please enter your name: \n')
+    time.sleep(1)
+    typingPrint(f'Lovely to meet you, ' + Fore.LIGHTMAGENTA_EX + f'{name}!\n' + Fore.RESET)
 
+def cycle_phase():
+  """
+  User can state whether or not they are on their period.
+  Will be provided with relevant questions to answer.
+  """
+
+  while True:
+        option = input('Are you on your period? Y/N\n')
+        if option == 'y' or option == 'Y':
+          typingPrint("We'll ask you some questions about the symptoms you're experiencing today...\n")
+        elif option == 'n' or option == 'N':
+          typingPrint("We'll ask you some questions about any symptoms you may be experiencing today\n")
+        else:
+          typingPrint(f'{option} is not a valid input. Please enter either Y or N...\n')
+
+# def validate_name_input(values):
+#     """
+#     Validates that a name has been inputed by the user.
+#     """  
+#     try:
+#         if len(values) < 1 and values.is_not:
+#             raise ValueError(
+#                 'We need your name to accurately track your symptoms!'
+#             )
+#     except ValueError as e:
+#         typingPrint('Please enter your name: ')
+
+# get_user_name()
+# validate_name_input()
+
+# def main():
+#     """
+#     contains all the functions for the program
+#     """
+#     get_user_name()
+
+print(Fore.LIGHTMAGENTA_EX + '     -------------------------------')
+print('  -------------------------------------')
+print('--------' + Fore.RED + ' Cycle Changes Tracker ' + Fore.LIGHTMAGENTA_EX + '----------')
+print('  -------------------------------------')
+print('     -------------------------------\n' + Fore.RESET)
 get_user_name()
+cycle_phase()
