7c99fee610d448a792ddf90e59b870c441226ddf
YasmineBB <y.bourhabi@gmail.com>
2022-04-05 14:33:35 +0000

    Add functions to check the user's pain and flow levels and if they have experienced those levels before. Input is checked and validated.

--- run.py
+++ run.py
@@ -106,49 +106,40 @@ def cycle_phase():
         if option == 'y' or option == 'Y':
           typingPrint("We'll ask you some questions about the symptoms"
           " you're experiencing today...\n")
-          return on_period()
+          return on_period_pain()
         elif option == 'n' or option == 'N':
           typingPrint("We'll ask you some questions about any symptoms you may"
           " be experiencing today\n")
+          return exit()
         else:
           typingPrint(f'{option} is not a valid input! Please enter either Y or N...\n')
 
         return False
 
+# global pain_exp_before 
 
-def on_period():
+def on_period_pain():
     """
     User will be asked questions related to the beginning of their cycle.
     Modified to check user input is integer, printing ValueError if not, using https://stackoverflow.com/questions/42338468/how-to-check-if-number-is-string-or-integer-in-python-using-if-else
     """
 
-    global pain_exp_before
-
-    pain_scale = typingInput('Where would you place your level of pain on a scale of 0 - 10? \n')
+    pain_scale = typingInput("Where would you place the level of pain you're experiencing on a scale of 0 - 10? \n"
+    "(0) No Pain (10) Extremely Painful\n")
 
     try:
         integer = int(pain_scale)
 
         if integer > 0:
-            typingPrint("Sorry to here you're experiencing pain today. It's great "
+            typingPrint("Sorry to hear you're experiencing pain today. It's great "
             "that you're logging symptoms, lets hope to change that.\n")
-            return (pain_exp_before)
+            return pain_exp()
         elif integer == 0:
             typingPrint('No pain today? thats great! \n')
+            return pain_exp()
 
     except ValueError:
         typingPrint(f'{pain_scale} is not a valid input. Please choose a number from 0 - 10.\n')
-
-    
-    pain_exp_before = typingInput('Have you experienced this level of pain before? Y/N \n')
-
-    while True:
-        if pain_exp_before == 'y' or pain_exp_before == 'Y' or pain_exp_before == 'n' or pain_exp_before == 'N':
-            typingPrint('Okay. This change has been logged.')
-            return flow_level
-        else:
-          return
-
 
     # while True:
     #     pain_scale = int(typingInput('Where would you place your level of pain on a scale of 0 - 10?\n'))
@@ -161,6 +152,42 @@ def on_period():
     #         typingPrint(f'{pain_scale} is not a valid input. Please choose a number from 0 - 10.\n')
         # else:
         #     return exit()
+        
+def pain_exp():
+      
+    while True:
+        pain_exp_before = typingInput('Have you experienced this level of pain before? Y/N \n')
+        if pain_exp_before == 'y' or pain_exp_before == 'Y' or pain_exp_before == 'n' or pain_exp_before == 'N':
+          typingPrint("Okay. We've logged this for you.\n")
+          return on_period_flow_level()
+        else:
+          typingPrint(f'{pain_exp_before} is not a valid input! Please enter either Y or N...\n')
+          continue
+
+
+
+def on_period_flow_level():
+
+    while True:
+        flow_level = typingInput('How would you describe your flow today on a scale of 1 - 5? \n'
+        '(0) None, (1) Very Light (2) Light (3) Medium (4) Heavy (5) Very Heavy?\n')
+        if int(flow_level) >= 0 and int(flow_level) <= 5:
+          print("Okay. We've logged this for you.\n")
+          return flow_exp()
+        else:
+          print(f'{flow_level} is not a valid input. Please input a number between 0 and 5.')
+          
+
+def flow_exp():
+
+    while True:
+        flow_exp_before = typingInput('Have you experienced this level of flow before? Y/N \n')
+        if flow_exp_before == 'y' or flow_exp_before == 'Y' or flow_exp_before == 'n' or flow_exp_before == 'N':
+          typingPrint("Okay. We've logged this for you.\n")
+          return exit()
+        else:
+          typingPrint(f"{pain_exp_before} is not a valid input! Please enter either Y or N...\n")
+          continue
 
 
 def any_other_phases():
@@ -170,7 +197,7 @@ def any_other_phases():
     """
 
 def exit():
-  print('Thank you for logging your symptoms today. Have a great day and see you tommorow!')
+  print("Thank you for logging your symptoms today. Have a great day and see you tommorow!\n")
   start()
 
 
@@ -211,5 +238,6 @@ def start():
 start()
 name = get_user_name()
 update_name(name)
-on_period()
+flow_exp()
+# on_period()
 exit()
